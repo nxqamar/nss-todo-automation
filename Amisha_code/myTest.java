df0c9aa57e2b075bbc340b975a67e6189c924b3c
amisha16 <iamgulu28@gmail.com>
2016-06-29 18:57:09 -0700

    myTest.java is updated with remaining assignment and TestCases.xlsx is added for testcases

--- Amisha_code/myTest.java
+++ Amisha_code/myTest.java
@@ -1,5 +1,5 @@
 
-/*Main Java Program to read URL and convert jason to java object and derive a result */
+/*Main Java Program to read URL and convert JSON to java object and Extract various Result */
 
 package TestAssignment;
 import TestAssignment.TodoJava;
@@ -12,9 +12,13 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -31,8 +35,8 @@ public class myTest {
          BufferedReader in = new BufferedReader(new InputStreamReader(
                                     conn.getInputStream()));
          
-        // Print Response Value from Server
-       /* String inputLine;
+       /* //Print Response Value from Server
+        String inputLine;
         while ((inputLine = in.readLine()) != null) 
             System.out.println(inputLine);
         in.close();
@@ -45,38 +49,91 @@ public class myTest {
 		while((read=in.read(chars))!=-1)
 			buffer.append(chars,0,read);
 
-		// json to java object parsing
-		
-        Gson gson = new GsonBuilder().create();
+		// JSON to java object parsing
+		Gson gson = new GsonBuilder().create();
         TodoJava[] tobj = gson.fromJson(buffer.toString(), TodoJava[].class);
-        TodoJava[] arr = gson.fromJson(buffer.toString(), TodoJava[].class);
-               
-        // Task : Find how many tasks do not have "category" assigned
-        int count=0;
+     
+        // TASK ::  Find how many tasks do not have "category" assigned
+       int count=0;
         for(int i=0;i<tobj.length;i++){
         	if(tobj[i].getCategory().isEmpty()) {
             		count = count+1;
             		}
         }
-        System.out.println("Empty Category count  is :: "+count);	
-       
+        System.out.println("Empty Category count  is :: "+count);
         
-      //  Aggregate and print only "task names"
-        
-      /*  for(int i=0;i<tobj.length;i++){
+     // TASK :: Count and validate the number of tasks
+        int j=0;
+        for(int i=0;i<tobj.length;i++){
+        	if(tobj[i].getStatus().contains("c")) {
+            		j++;
+        	}
+        }
+      System.out.println("\nCompleted (strike out) task are :: "+j);
+
+        // TASK ::  Aggregate and print only "task names"
+      	System.out.println("\nTotal task in list are : ") ;
+        int cnt=0;
+        for(int i=0;i<tobj.length;i++){
+        	cnt=cnt+1;
         	System.out.println(tobj[i].getTaskname());
         }
-      */  
+       // System.out.println("\n Total Number of task are :: "+ cnt) ;
+   
         
-        //Count and validate the number of tasks
+        // TASK :  Read API response and print tasks in descending "due date" order        
+      	
+        ArrayList<TodoJava> arList = new ArrayList<TodoJava> (Arrays.asList(tobj)); /*Convert array of object  to ArrayList */
+        			/*int count1= arList.size();	System.out.println(count1);*/
+       System.out.println("\n sorted list of task is :" );	
+       Collections.sort(arList, new Comparator <TodoJava> (){
+          
+            public int compare(TodoJava e1, TodoJava e2) {
+              
+            	if (!(e1.getDate().equals(e2.getDate()))){
+                	System.out.println( e1.getDate()+ "" + e1.getTaskname() + "\n");
+                    return -1;
+                } else                 	
+                    return 1;
+            }
+        });
+
         
         
-       // Read API response and print tasks in descending "due date" order 
-        Type collectionType = new TypeToken<List<TodoJava>>() {
-        }.getType();
-        List<TodoJava> list1 = gson.fromJson(buffer.toString(), collectionType);
-        	//System.out.println(list1.get(0).getDate());
-        	
-	}
+       /* ArrayList<TodoJava> arList1 = new ArrayList<TodoJava> (Arrays.asList(tobj));
+       Collections.sort(arList1, new Comparator <TodoJava> (){
+          
+            public int compare(TodoJava e1, TodoJava e2) {
+            	
+            	return e2.getDate().compareTo(e1.getDate());
+            	
+            	            	if (e1.getDate().equals(e2.getDate())){
+                	System.out.println( e2.getDate()); // "" + data2.getTaskname() + "\n");
+                    return -1;
+                } else {
+                	System.out.println(data2.getDate()+ "" + data2.getTaskname() + "\n");
+                    return 1;
+                   
+                }
+            }
+           
+
+        });
+*/        
+
+            	/*//if (!(data2.getDate().equals(data1.getDate())))
+            	 if (Integer.getInteger(data1.getID())< Integer.getInteger(data2.getID())){
+                	System.out.println(data1.getID());
+                    return -1;
+            	               }
+            	if (data1.getDate().toString().compareTo(data2.getDate().toString())>1){
+                	System.out.println( data2.getDate() ); 
+                    return -1;
+                } else 
+                	return 1;
+            }   */
+ 
+             
+}
 
 }
